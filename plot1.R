373b66c6fc2f79605390f884d8ec2c4e5e032006
MariiaUkrainets <47226385+MariiaUkrainets@users.noreply.github.com>
2019-11-30 23:26:27 +0200

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,13 @@
+
+df <- read.table('household_power_consumption.txt', sep = ';', header = TRUE, na.strings = '?')
+new_df <- subset(df, Date == '1/2/2007' | Date == '2/2/2007')
+
+
+new_df$DateTime <- paste(new_df$Date, new_df$Time)
+new_df$Time <- strptime(paste(new_df$Date, new_df$Time), '%d/%m/%Y %H:%M:%S')
+new_df$Date <- as.Date(new_df$Date, '%d/%m/%Y')
+
+
+png('plot1.png', width=480, height=480)
+hist(new_df$Global_active_power, col = 'red', xlab = 'Global Active Power (kilowatts)', ylab = 'Frequency', main = 'Global Active Power')
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,25 @@
+
+df <- read.table('household_power_consumption.txt', sep = ';', header = TRUE, na.strings = '?')
+new_df <- subset(df, Date == '1/2/2007' | Date == '2/2/2007')
+
+
+new_df$DateTime <- paste(new_df$Date, new_df$Time)
+new_df$Time <- strptime(paste(new_df$Date, new_df$Time), '%d/%m/%Y %H:%M:%S')
+new_df$Date <- as.Date(new_df$Date, '%d/%m/%Y')
+
+png('plot4.png', width=480, height=480)
+par(mfrow = c(2, 2))
+# subplot 1
+plot(new_df$Time, new_df$Global_active_power, type = 'l', xlab = '', ylab = 'Global Active Power')
+# subplot 2
+plot(new_df$Time, new_df$Voltage, type = 'l', xlab = 'datetime', ylab = 'Voltage')
+# subplot 3
+plot(new_df$Time, new_df$Sub_metering_1, type = 'l', xlab = '', ylab = 'Energy sub metering')
+lines(new_df$Time, new_df$Sub_metering_2, type = 'l', col = 'red')
+lines(new_df$Time, new_df$Sub_metering_3, type = 'l', col = 'blue')
+colour <- c('black', 'red', 'blue')
+leg <- c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3')
+legend('topright', lty = 1, col = colour, legend = leg)
+# subplot 4
+plot(new_df$Time, new_df$Global_reactive_power, type = 'l', xlab = 'datetime', ylab = 'Global Rective Power')
+dev.off()
